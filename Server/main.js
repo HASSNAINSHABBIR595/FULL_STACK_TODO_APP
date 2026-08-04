762b68b8617fb1800a6c1bef25e90334d57deb8c
Hasnain <hassnainshabbir595@gmail.com>
2026-08-04 13:51:55 +0500

    change link

--- Server/main.js
+++ Server/main.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 dotenv.config();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "gentle-possibility-production-3ad7.up.railway.app",
     credentials: true,
   }),
 );
